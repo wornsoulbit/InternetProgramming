2b0e15cb863401d10ec7062dd69ee68b3ab1fdad
Alex Vasil <alexandervasil840@gmail.com>
2020-11-09 14:08:16 -0500

    pushing assignments/labs to git

---
+++ Labs/Lab3/code.js
@@ -0,0 +1,6 @@
+var bannerImages;
+
+function setUpBannerImages() {
+    bannerImages = new Array('Images\1hYZdLu.jpg', 'Images\5SYeY9l.jpg', 'Images\CqwQVj1.jpg', 'Images\cxKxHG2.jpg', 'Images\download.jpg');
+
+}
